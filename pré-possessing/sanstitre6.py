11b45db505d9d7f209e28f94b9506ef4da311315
idene-hug <wouagfacklaura19931@gmail.com>
2025-06-26 00:18:39 +0200

    ajout de sanstitre6

---
+++ pré-possessing/sanstitre6.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 26 00:15:13 2025
+
+@author: User
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 21 03:21:38 2025
+
+@author: User
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 21 02:47:02 2025
+
+@author: User
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 17 17:08:38 2025
+
+@author: User
+"""
+
+x,y=3,5
+print(x)
+print(y)
+#Arithmétiques
+print(x==y)
+print(x<y)
+print(x!=y )
+print(x+y)
+print(x-y)
+print(x*y)
+print(y**x)
+def e_potentielle(masse,hauteur,g):
+    E=masse*hauteur*g
+    print(E,"joules")
+    return E
+résultat=e_potentielle(20,5,9.81)
+#exercice
+def e_potentielle(masse,hauteur,g,E_limite):
+    E=masse*hauteur*g
+    if(E<E_limite):
+        print(E,"joules")
+    else:
+        print("bonjour")
+    return E
+résultat=e_potentielle(20,5,9.81,800)
+#liste
+liste_1=[1,2,3,4,5,6,7]
+liste_2=["paris","londre","iles"]
+liste_3=[liste_1,liste_2]
+liste_4=[]
+#tuple
+tuple_1=(1,2,3,9,5)
